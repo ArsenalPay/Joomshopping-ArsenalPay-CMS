20c4449123eff014a9f732b08a327df54ceccbd8
Natalya Li <pro.nata90@yandex.ru>
2014-11-20 20:49:21 +1100

    rm components

--- components/com_jshopping/payments/pm_arsenalpay/paymentform.php
+++
@@ -1,7 +0,0 @@
-<?php ('_JEXEC') or die('Restricted access'); 
-?>
-<script type="text/javascript">
-function check_<?php print $paymentPluginClass; ?>(){
-    jQuery('#payment_form').submit();
-}
-</script>
