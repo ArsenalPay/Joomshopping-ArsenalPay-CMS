34f6d84d435b8defc41cf1b0dd8b6e5125ec02fc
Natalya <pro.nata90@yandex.ru>
2015-02-11 11:49:11 +1100

    Update paymentform.php

--- components/com_jshopping/payments/pm_arsenalpay/paymentform.php
+++ components/com_jshopping/payments/pm_arsenalpay/paymentform.php
@@ -1,4 +1,12 @@
-<?php ('_JEXEC') or die('Restricted access'); 
+<?php
+/**
+* @version      1.0.0
+* @author       ArsenalPay
+* @package      Jshopping
+* @copyright    Copyright (C) 2014 arsenalpay.ru. All rights reserved.
+* @license      GNU/GPL
+*/
+defined ('_JEXEC') or die('Restricted access'); 
 ?>
 <script type="text/javascript">
 function check_<?php print $paymentPluginClass; ?>(){
